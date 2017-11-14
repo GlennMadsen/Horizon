6f95130c87deae0827414b572cceb05aee84e516
Glenn Madsen <gclam11@gmail.com>
2017-11-13 17:47:03 -0700

    Mostly cosmetic changes

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import DatePicker from './DatePicker/DatePicker.js';
 import PopupButton from './Popup/PopupButton.js';
 
+document.querySelector("body").style.background = "url(http://lorempixel.com/" + Math.min(window.innerWidth,1920) + "/" + Math.min(window.innerHeight, 1920) + "?r="+Math.random()+")";
+
 ReactDOM.render(<PopupButton lable="Choose A Date" onSubmit={(moment) => alert(moment.format("MMMM DD, YYYY"))}><DatePicker time={Date.now()} /></PopupButton>, document.querySelector("#app"));
 registerServiceWorker();
